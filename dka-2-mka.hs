aa659a7201ada140d94e4da77f6cd3aacc89397e
SergeyPanov <sergeypanov@Sergeys-MacBook-Pro.local>
2018-02-10 17:19:35 +0100

    Inner representation

--- dka-2-mka.hs
+++ dka-2-mka.hs
@@ -2,18 +2,39 @@ module Main where
 import System.Environment
 import System.IO
 import System.IO.Error
-import qualified Data.Set as Set  
+import qualified Data.Set as Set
 
+-- Hold input parameters
 dispatch :: [([Char], String -> IO ())]
 dispatch = [
     ("-i", readAndPrint),
     ("-t", minimize)
     ]
 
+-- Split string on comma ','
+split :: String -> Char -> [String]
+split [] delim = [""]
+split (c:cs) delim
+   | c == delim = "" : rest
+   | otherwise = (c : head rest) : tail rest
+   where
+       rest = split cs delim
+
+
+-- Create tuple with rule
+tuplify3 :: [t] -> (t, t, t)
+tuplify3 [q, a, p] = (q, a, p)
+
 -- Execute reading and printing
 readAndPrint :: String -> IO()
-readAndPrint x = do
-    putStr $ x ++ " readAndPrint"
+readAndPrint input = do
+    let lns = lines input
+        (states:start:finits:rules) = lns
+    print $ split states ','
+    print $ split start ','
+    print $ split finits ','
+    mapM print rules
+    print rules
 
 
 -- Execute minimization
@@ -21,30 +42,29 @@ minimize :: String -> IO()
 minimize x = do
     putStr $ x ++ " minimize"
 
-
+-- Read DKA from the file
 readFromFile :: String -> IO()
-readFromFile x = putStrLn $ x ++  " read from file"
-
--- readFromStdIn :: IO()
+readFromFile file = do
+    withFile  file ReadMode (\handle -> do
+                                            contents <- hGetContents handle
+                                            readAndPrint contents
+                                            )
+-- Read DKA from stdin
+readFromStdIn :: IO()
 readFromStdIn = do
         contents <- getContents
         putStrLn contents
 
--- readDKA :: IO() -> [t]
--- readDKA handle = do
 
 main = do
     (command:source) <- getArgs
-    putStrLn command
+    putStrLn $ "The command " ++  command
 
     let (Just action) = lookup command dispatch
 
     if length source == 0
         then readFromStdIn
-        else do
-            withFile  (head source) ReadMode (\handle -> do
-                                            contents <- hGetContents handle
-                                            putStrLn contents
-                                            )
+        else do readFromFile (head source)
+            
 
-    putStrLn "Hello"
+    putStrLn "End main"
